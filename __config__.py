d00d262773db42ab9aab47e1755077a0ba40422f
Hasib Zunair <hasibzunair@gmail.com>
2018-06-27 20:08:56 +0600

    code cleaning and commenting

--- __config__.py
+++ __config__.py
@@ -1,12 +1,13 @@
 import os.path
 
+# file paths to respective files
 CASCADE_FILE = os.path.expandvars(".\\res\\haarcascade_frontalface_default.xml")
 DATASETS_DIR = os.path.expandvars('.\\data')
 TRAINED_DATA = os.path.expandvars('.\\training\\trainer.yml')
 STOP_KEYCODE = 27
 TRAINING_SAMPLE_COUNT = 100
 
+# params
 SCALE = 1.2
 NEARBY = 6
-
 WINDOW_TITLE = "cv2.imshow()  # ESC to stop"
